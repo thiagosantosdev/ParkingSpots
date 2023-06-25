144f0a75dc91c134693a5cf540d791365905078e
thiago-dev <thiagosantosjss@gmail.com>
2023-06-24 21:02:13 -0300

    commit parkingspots

---
+++ parking-control/src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -0,0 +1,18 @@
+package com.api.parkingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControlApplication.class, args);
+		
+		System.out.println("Parking");
+
+
+	}
+
+	
+}
---
+++ parking-control/src/main/java/com/api/parkingcontrol/repository/ParkingSpotRepository.java
@@ -0,0 +1,13 @@
+package com.api.parkingcontrol.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.parkingcontrol.model.ParkingSpots;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpots, UUID> {
+
+}
